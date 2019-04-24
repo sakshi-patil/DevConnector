1f3f849c521b89eabec73620622f1181f3405384
sakshi-patil <sakshipatil2805@gmail.com>
2019-04-25 05:22:41 +0530

    route files with express router

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+  'mongodb://sakshi:sakshi123@ds153394.mlab.com:53394/devconnector'
+};
+
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+// include local files
+//const keys = require('./config/keys')
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello'));
+//DB config
+const db = require('./config/keys').mongoURI;
+
+
+// mongodb connection
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+
+app.get("/",(req, res) => res.send("Hello World"));
+
+
+
+//use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 
 const port = process.env.PORT || 5000;
